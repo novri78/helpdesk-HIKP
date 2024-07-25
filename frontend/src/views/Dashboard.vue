e10c88642b691adb4c76e2d1c3b0ad86268e7bc5
Novri Marizal <novri.marizal24@gmail.com>
2024-07-25 14:57:53 +0700

    add basic code for Dashboard

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <header></header>
+    <main>
+        <article>
+            <h1>Dashboard</h1>
+        </article>
+    </main>
+    <footer>
+        <p>&copy; 2024 - Helpdesk HIKP. Hak cipta dilindungi.</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard'
+}
+</script>
