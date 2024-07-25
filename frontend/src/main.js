50e626e5fa3562e74a0b14c5056225cb69db46e4
Novri Marizal <novri.marizal24@gmail.com>
2024-07-25 14:37:57 +0700

    edit main.js to setup axios to connect API, bootstrap for styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,28 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 
-createApp(App).use(store).use(router).mount('#app')
+import { createBootstrap } from 'bootstrap-vue-next';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
+// Set base URL for Axios
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8080/api',
+    timeout: 180000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+// Initialize Vue app
+const app = createApp(App);
+
+// Make axios accessible globally
+app.config.globalProperties.$axios = {...axiosInstance}; 
+
+app.use(store);
+app.use(router);
+app.use(createBootstrap({ components: true, directives: true }));
+app.mount('#app')
