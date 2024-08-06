246fd447980a3104e81c40a0f9912064414ec113
Novri Marizal <novri.marizal24@gmail.com>
2024-08-06 21:30:57 +0700

    Add AuthService.java, LoginRequestDTO.java, LoginResponseDTO.java dan enhance AuthController.java & AuthenticationRequest.java

--- backend/src/main/java/com/example/helpdesk/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/helpdesk/backend/controller/AuthController.java
@@ -30,13 +30,13 @@ public class AuthController {
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail (), authenticationRequest.getPassword())
             );
         } catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
         }
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail ());
 
         final String jwt = jwtUtil.generateToken(userDetails);
 
