6b0033acad6633ebf18627b40b04f84adf103b7f
Novri Marizal <novri.marizal24@gmail.com>
2024-08-06 20:42:56 +0700

    Enhance SecurityConfig & UserRepository

--- backend/src/main/java/com/example/helpdesk/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/helpdesk/backend/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtRequestFilter jwtRequestFilter;
     private final UserDetailsService userDetailsService;
