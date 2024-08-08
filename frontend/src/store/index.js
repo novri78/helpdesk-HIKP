11ad856371046a1014f71b23aa33895bdf9aad7f
Novri Marizal <novri.marizal24@gmail.com>
2024-08-08 16:33:22 +0700

    Modified store/index.js to handle token

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,12 +8,9 @@ export default createStore({
     token: null,
   },
   mutations: {
-    // SET_LOGIN(state, { user, token}) {
-    //   state.user = user;
-    //   state.token = token;
-    // },
-    SET_LOGIN(state, user) {
+    SET_LOGIN(state, {user,token}) {
       state.user = user;
+      state.token = token;
     },
     SET_LOGOUT(state) {
       state.user = null;
@@ -21,14 +18,28 @@ export default createStore({
     },
   },
   actions: {
-    async login({ commit }, { user, token}) {
-      try {
-        const resp = await this.$axios.post(`/auth/login`, credentials);
-        console.log('value_login', resp.data)
+    // async login({ commit }, { user, token}) {
+    //   try {
+    //     const resp = await this.$axios.post(`/auth/login`, credentials);
+    //     console.log('value_login', resp.data)
 
-        commit('SET_LOGIN', { user, token })
-        cookie.set('users', JSON.stringify({ user, token }), { expires: 1 });
+    //     commit('SET_LOGIN', { user, token })
+    //     cookie.set('users', JSON.stringify({ user, token }), { expires: 1 });
         
+    //   } catch (error) {
+    //     commit('SET_LOGOUT');
+    //     throw error;
+    //   }
+    // },
+    async login({ commit }, credentials) {
+      try {
+        const resp = await axios.post(`/auth/login`, credentials);
+        const { token, user } = resp.data;
+
+        commit('SET_LOGIN', user);
+        commit('SET_TOKEN', token);
+        cookie.set('token', token, { expires: 1 });
+        cookie.set('user', JSON.stringify(user), { expires: 1 });
       } catch (error) {
         commit('SET_LOGOUT');
         throw error;
@@ -36,16 +47,20 @@ export default createStore({
     },
     logout({ commit }) {
       commit('SET_LOGOUT');
-      cookie.remove('userdata');
+      cookie.remove('user');
+      cookie.remove('token');
     },
-    checkAuth( {commit} ) {
-      const cookieData = cookie.get('users');
-      if (cookieData) {
-        try {
-
-        } catch (e) {
-          console.error("Failed to parse cookie data:", e);
-        }
+    checkAuth({ commit }) {
+      const token = cookie.get('token');
+      if (token) {
+        commit('SET_TOKEN', token);
+        this.dispatch('fetchUserData');
+      }
+    },
+    fetchUserData({ commit }) {
+      const user = JSON.parse(cookie.get('user'));
+      if (user) {
+        commit('SET_LOGIN', user);
       }
     },
   },
