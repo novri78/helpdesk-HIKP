3e20b9b4709de863e48f6b62291a440c222808c1
Novri Marizal <novri.marizal24@gmail.com>
2024-08-14 20:48:32 +0700

    Add NavHelpdesk and SidebarHelpdesk

---
+++ frontend/src/components/NavHelpDesk.vue
@@ -0,0 +1,205 @@
+<template>
+  <nav class="navbar">
+    <button class="navbar-toggler" @click="toggleMenu">
+      <span class="navbar-toggler-icon">&#9776;</span>
+    </button>
+    <div :class="['navbar-collapse', { show: isMenuOpen }]">
+      <ul class="navbar-nav ml-auto">
+        <li v-if="user.name" class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" @click="toggleDropdown">{{
+            user.name
+          }}</a>
+          <ul class="dropdown-menu" :class="{ show: isDropdownOpen }">
+            <li class="dropdown-item" @click="confirmLogout">Logout</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import Cookies from "js-cookie";
+
+export default {
+  name: "Nav",
+  data() {
+    return {
+      user: {
+        name: "",
+      },
+      isMenuOpen: false,
+      isDropdownOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    ...mapActions(["logout"]),
+    confirmLogout() {
+      if (confirm("Are you sure you want to logout?")) {
+        this.handleLogout();
+      }
+    },
+    handleLogout() {
+      this.logout();
+      this.$router.push("/").then(() => {
+        setTimeout(() => {
+          return alert("You have been logged out.");
+        }, 300); // Delay of 100ms
+      });
+    },
+    getUserData() {
+      const cookieData = Cookies.get("userdata"); // Ambil data dari Cookies
+      console.log("value cookieData", cookieData);
+
+      if (cookieData) {
+        const parsedData = JSON.parse(cookieData); // Parse data JSON
+        this.user.name = parsedData.user.name;
+        console.log("this.user.name", this.user.name);
+        // if (parsedData && parsedData.name) {
+        //   this.user.name = parsedData.name; // Ambil nama user
+        //   console.log('this.user.name', this.user.name);
+        // } else if (parsedData && parsedData.userdata) {
+        //   this.user.name = parsedData.userdata.name; // Ambil nama dari userdata
+        //   console.log('this.user.name', this.user.name);
+        // }
+      }
+    },
+  },
+  mounted() {
+    this.getUserData();
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #007bff;
+  padding: 10px 5px;
+  height: 60px;
+  box-sizing: border-box;
+}
+
+.navbar-toggler {
+  background: none;
+  border: none;
+  font-size: 1.5rem;
+  color: #fff;
+  cursor: pointer;
+  display: none;
+}
+
+.navbar-toggler-icon {
+  display: inline-block;
+}
+
+.navbar-collapse {
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-end;
+  align-items: center;
+  transition: height 0.3s ease;
+  height: auto;
+}
+
+.navbar-collapse.show {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: #007bff;
+  width: 100%;
+  padding: 10px 0;
+}
+
+.navbar-nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+
+.nav-item {
+  position: relative;
+}
+
+.nav-link {
+  color: #fff;
+  text-decoration: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+@media (max-width: 768px) {
+  .navbar-toggler {
+    display: block;
+  }
+
+  .navbar-collapse {
+    display: none;
+    flex-direction: column;
+    align-items: flex-start;
+    background-color: #007bff;
+    width: 100%;
+    padding: 10px 0;
+  }
+
+  .navbar-collapse.show {
+    display: flex;
+  }
+
+  .nav-item {
+    width: 100%;
+    text-align: center;
+  }
+
+  .nav-link {
+    display: block;
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+  }
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: #fff;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 150px;
+  border: 1px solid #007bff;
+  border-radius: 5px;
+  display: none;
+}
+
+.dropdown-menu.show {
+  display: block;
+}
+
+.dropdown-item {
+  padding: 10px;
+  color: #007bff;
+  cursor: pointer;
+  &:hover {
+    background-color: #007bff;
+    color: #fff;
+  }
+}
+</style>
---
+++ frontend/src/components/SidebarHelpdesk.vue
@@ -0,0 +1,154 @@
+<template>
+  <div :class="['sidebar', { 'sidebar_on': toggleSwitch }]" id="toggle_sidebar">
+    <div class="toggle" @click="setToggle">
+      <i :class="toggleSwitch ? 'mdi mdi-menu' : 'mdi mdi-menu'"></i>
+    </div>
+    <div class="navbar-brand">
+      <a href="#">
+        <img src="" alt="Logo" class="navbar-logo">
+      </a>
+    </div>
+    <ul class="nav flex-column mt-4">
+      <router-link to="/dashboard" class="nav-link" exact>Dashboard</router-link>
+      <router-link to="/tickets" class="nav-link">Ticket</router-link>
+      <li class="nav-item">
+        <a class="nav-link" @click="toggleDropdown('users')">
+          List Users 
+          <i :class="dropdowns.users ? 'mdi mdi-chevron-up' : 'mdi mdi-chevron-down'"></i>
+        </a>
+        <ul v-if="dropdowns.users" class="sidebar-item-dropdown">
+          <li>
+            <router-link to="/register" class="dropdown-item">Register New User</router-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleSwitch: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      dropdowns: {
+        users: false,
+      },
+    };
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    }
+  },
+  methods: {
+    setToggle() {
+      this.$emit('toggle-sidebar');
+    },
+    toggleDropdown(name) {
+      this.dropdowns[name] = !this.dropdowns[name];
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import url('https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+
+.sidebar {
+  position: fixed;
+  z-index: 1001;
+  left: -200px;
+  top: 0;
+  width: 200px;
+  height: 100vh;
+  background-color: #007bff;
+  transition: left 0.3s ease-in-out;
+  padding-top: 60px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+  &.sidebar_on {
+    left: 0;
+  }
+
+  .nav-link {
+    margin-top: 0.6rem;
+    color: #fff;
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    &:hover {
+      color: #495057;
+      background-color: rgba(255, 255, 255, 0.1);
+      border-radius: 5px;
+    }
+  }
+
+  .toggle {
+    position: absolute;
+    right: -30px;
+    top: 10px;
+    width: 30px;
+    height: 50px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #007bff;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    i {
+      font-size: 24px;
+    }
+  }
+
+  .sidebar-item-dropdown {
+    background-color: #007bff;
+    list-style: none;
+    padding-left: 0;
+    .dropdown-item {
+      padding: 10px 15px;
+      color: #fff;
+      text-decoration: none;
+      display: block;
+      &:hover {
+        color: #495057;
+        background-color: rgba(255, 255, 255, 0.1);
+        border-radius: 5px;
+      }
+    }
+  }
+
+  .navbar-brand {
+    display: flex;
+    align-items: center;
+    margin: -35px 0 0 15px;
+  }
+
+  .navbar-logo {
+    width: 50px;
+    height: auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    left: -100%;
+    &.sidebar_on {
+      left: 0;
+    }
+  }
+  .toggle {
+    right: 0;
+  }
+}
+</style>
