e1a1170eabec77eb28d0096095fa1307d9148fb1
Novri Marizal <novri.marizal24@gmail.com>
2024-08-16 10:08:46 +0700

    Modified refactoring folder CORS

--- backend/src/main/java/com/example/helpdesk/backend/filter/MyConfiguration.java
+++ backend/src/main/java/com/example/helpdesk/backend/filter/MyConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.helpdesk.backend.constant;
+package com.example.helpdesk.backend.filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- backend/src/main/java/com/example/helpdesk/backend/filter/WebConfig.java
+++ backend/src/main/java/com/example/helpdesk/backend/filter/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.helpdesk.backend.constant;
+package com.example.helpdesk.backend.filter;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
